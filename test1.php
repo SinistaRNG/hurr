26b813f820de742368cafbe4143c655984de5c8c
rather be dead <swhansen@mtu.edu>
2014-11-29 08:41:50 -0500

    :\

--- test1.php
+++ test1.php
@@ -1,1 +1,3 @@
+<html>
 <form name="form1" method="post" action="test2.php"> Username: <input type="text" name="user"><br>Email: <input type="text" name="mail"><br>Experience: <select name="exp"> <option value="beginner">Beginner</option> <option value="intermediate">Intermediate</option> <option value="advanced">Advanced</option> </select><br> <input type="submit" name="Submit" value="Sign Up"> </form>
+</html>
